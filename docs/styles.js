ce77ec2d882cac2a91038425b3a4ae67093c5b25
Killian Perea Ruiz <killianperearuiz@MacBook-Pro-de-Killian.local>
2018-10-02 15:41:19 +0200

    add new members + footer + tooltips

--- docs/styles.js
+++ docs/styles.js
@@ -539,7 +539,7 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Workspace\frontend-backend-server-team\src\styles.scss */"./src/styles.scss");
+module.exports = __webpack_require__(/*! /Users/killianperearuiz/workspace/frontend-backend-server-team/src/styles.scss */"./src/styles.scss");
 
 
 /***/ })
--- src/constants/technologies.constants.ts
+++ src/constants/technologies.constants.ts
@@ -4,37 +4,53 @@ export const Technologies = {
     frontend: [
         {
             name: 'angular',
-            logo: 'assets/img/logos/angular.svg'
+            logo: 'assets/img/logos/angular.svg',
+            tooltip: 'Angular',
+            link: 'https://angular.io/'
         },
         {
             name: 'ionic',
-            logo: 'assets/img/logos/ionic.svg'
+            logo: 'assets/img/logos/ionic.svg',
+            tooltip: 'Ionic',
+            link: 'https://ionicframework.com/'
         },
         {
             name: 'sass',
-            logo: 'assets/img/logos/sass.svg'
+            logo: 'assets/img/logos/sass.svg',
+            tooltip: 'SASS',
+            link: 'https://sass-lang.com/'
         },
         {
             name: 'cordova',
-            logo: 'assets/img/logos/cordova.png'
+            logo: 'assets/img/logos/cordova.png',
+            tooltip: 'Cordova',
+            link: 'https://cordova.apache.org/'
         }
     ] as TechnologyModel[],
     backend: [
         {
             name: 'node',
-            logo: 'assets/img/logos/node.png'
+            logo: 'assets/img/logos/node.png',
+            tooltip: 'Node',
+            link: 'https://nodejs.org/en/'
         },
         {
             name: 'docker',
-            logo: 'assets/img/logos/docker.svg'
+            logo: 'assets/img/logos/docker.svg',
+            tooltip: 'Docker',
+            link: 'https://www.docker.com/'
         },
         {
             name: 'mongodb',
-            logo: 'assets/img/logos/mongodb.svg'
+            logo: 'assets/img/logos/mongodb.svg',
+            tooltip: 'Mongo DB',
+            link: 'https://www.mongodb.com/en'
         },
         {
             name: 'java',
-            logo: 'assets/img/logos/java.svg'
+            logo: 'assets/img/logos/java.svg',
+            tooltip: 'Java',
+            link: 'https://java.com/en/'
         }
     ] as TechnologyModel[]
 };
