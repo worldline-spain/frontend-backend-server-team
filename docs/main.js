d2209af7abf7615b521b8acb905d1c2ec314b70e
EricTorresP <eric.torres@worldline.com>
2018-12-18 13:52:13 +0100

    ADD: main constants members

--- docs/main.js
+++ docs/main.js
@@ -705,294 +705,338 @@ var TechnologiesComponent = /** @class */ (function () {
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "TeamMembers", function() { return TeamMembers; });
 var TeamMembers = [
-    {
-        name: 'Adrian Borrego Martinez',
-        photo: 'assets/img/members/AdrianBorrego.png',
-        description: 'Haters gonna hate',
-        position: 'DEVELOPER',
-        social: [
-            {
-                icon: 'fab fa-github',
-                url: 'https://github.com/adryy5'
-            }
-        ]
-    },
-    {
-        name: 'Alberto Iglesias Gonzalo',
-        photo: 'assets/img/members/AlbertoIglesias.jpg',
-        description: 'Metrosexual part-time and full-time thinker.',
-        position: 'DEVELOPER',
-        social: [
-            {
-                icon: 'fab fa-linkedin-in',
-                url: 'https://www.linkedin.com/in/alberto-iglesias-gonzalo-023511a9/'
-            },
-            {
-                icon: 'fab fa-github',
-                url: 'https://github.com/srkapi'
-            }
-        ]
-    },
-    {
-        name: 'Alejandro Rivas Moreira',
-        photo: 'assets/img/members/AlejandroRivas.jpg',
-        description: 'It is over anakin, I have the high ground',
-        position: 'DEVELOPER',
-        social: [
-            {
-                icon: 'fab fa-github',
-                url: 'https://github.com/AlexHikari/'
-            }
-        ]
-    },
-    {
-        name: 'Alejandro Verbo Macias',
-        photo: 'assets/img/members/AlejandroVerbo.jpg',
-        description: 'There are two rules for success: 1) Never say everything you know',
-        position: 'DEVELOPER',
-        social: [
-            {
-                icon: 'fab fa-linkedin-in',
-                url: 'https://www.linkedin.com/in/alejandro-verbo-0822227/'
-            },
-            {
-                icon: 'fab fa-github',
-                url: 'https://github.com/averbo'
-            }
-        ]
-    },
-    {
-        name: 'Ana Avilés Herrero',
-        photo: 'assets/img/members/AnaAviles.jpg',
-        description: 'No information found',
-        position: 'DEVELOPER',
-        social: [
-            {
-                icon: 'fab fa-linkedin-in',
-                url: 'https://www.linkedin.com/in/ana-avil%C3%A9s-b228079/'
-            },
-            {
-                icon: 'fab fa-github',
-                url: 'https://github.com/aAvilesh'
-            }
-        ]
-    },
-    {
-        name: 'Ana María Castaño Díaz',
-        photo: 'assets/img/members/AnaMariaCastano.png',
-        description: 'My richness is life',
-        position: 'DEVELOPER',
-        social: [
-            {
-                icon: 'fab fa-github',
-                url: 'https://github.com/AnaaMaria25'
-            }
-        ]
-    },
-    {
-        name: 'Antonio Fiñana Sanchez',
-        photo: 'assets/img/members/AntonioFinana.jpg',
-        description: 'Nice to meet myself',
-        position: 'DEVELOPER',
-        social: [
-            {
-                icon: 'fab fa-linkedin-in',
-                url: 'https://www.linkedin.com/in/antonio-fi%C3%B1ana-30a40b49/?originalSubdomain=es'
-            },
-            {
-                icon: 'fab fa-github',
-                url: 'https://github.com/afinana'
-            }
-        ]
-    },
-    {
-        name: 'Cristina Figueres Nuñez',
-        photo: 'assets/img/members/CristinaFigueres.jpg',
-        description: ' I am too old for this shit... ',
-        position: 'DEVELOPER',
-        social: [
-            {
-                icon: 'fab fa-linkedin-in',
-                url: 'https://es.linkedin.com/in/cristinafigueres'
-            },
-            {
-                icon: 'fab fa-github',
-                url: 'https://github.com/cristinafigueres'
-            }
-        ]
-    },
-    {
-        name: 'Dani Bautista Miralles',
-        photo: 'assets/img/members/DaniBautista.png',
-        description: 'There is no spoon',
-        position: 'DEVELOPER',
-        social: [
-            {
-                icon: 'fab fa-linkedin-in',
-                url: 'https://www.linkedin.com/in/daniel-bautista-miralles-14707181/'
-            },
-            {
-                icon: 'fab fa-github',
-                url: 'https://github.com/Dabami'
-            }
-        ]
-    },
-    {
-        name: 'Eric Álvarez Chinchilla',
-        photo: 'assets/img/members/EricAlvarez.jpg',
-        description: '#nofilter #frontend #developer #worldline',
-        position: 'DEVELOPER',
-        social: [
-            {
-                icon: 'fab fa-linkedin-in',
-                url: 'https://www.linkedin.com/in/eric-alvarez-chinchilla/'
-            },
-            {
-                icon: 'fab fa-github',
-                url: 'https://github.com/ealch'
-            }
-        ]
-    },
-    {
-        name: 'Jairo Costa Barriga',
-        photo: 'assets/img/members/JairoCosta.jpg',
-        description: 'Magic is the science we do not understand yet',
-        position: 'SCHOLARSHIP HOLDER',
-        social: [
-            {
-                icon: 'fab fa-linkedin-in',
-                url: 'https://www.linkedin.com/in/jairocostaes'
-            },
-            {
-                icon: 'fab fa-github',
-                url: 'https://github.com/soyJairoCosta'
-            }
-        ]
-    },
-    {
-        name: 'Jordi Escudero Subirana',
-        photo: 'assets/img/members/JordiEscudero.jpg',
-        description: 'The any type in Typescript was build for me!',
-        position: 'HEAD',
-        social: [
-            {
-                icon: 'fab fa-linkedin-in',
-                url: 'https://www.linkedin.com/in/jordi-escudero/'
-            },
-            {
-                icon: 'fab fa-github',
-                url: 'https://github.com/jordiescudero'
-            }
-        ]
-    },
-    {
-        name: 'Jorge Oíz Acosta',
-        photo: 'assets/img/members/JorgeOiz.jpg',
-        description: 'To change is to progress',
-        position: 'DEVELOPER',
-        social: [
-            {
-                icon: 'fab fa-linkedin-in',
-                url: 'https://www.linkedin.com/in/jorge-oiz-acosta-80a34b57/'
-            },
-            {
-                icon: 'fab fa-github',
-                url: 'https://github.com/JorgeOiz'
-            }
-        ]
-    },
-    {
-        name: 'Jose Luis Pleguezuelos Aguilera',
-        photo: 'assets/img/members/JoseLuisPleguezuelos.jpg',
-        description: 'Do not take life seriously; total, you will not live out of it',
-        position: 'DEVELOPER',
-        social: [
-            {
-                icon: 'fab fa-linkedin-in',
-                url: 'https://www.linkedin.com/in/jose-luis-pleguezuelos-aguilera'
-            }
-        ]
-    },
-    {
-        name: 'Juan Belmonte Rodríguez',
-        photo: 'assets/img/members/JuanBelmonte.jpg',
-        description: '75% water',
-        position: 'DEPUTY',
-        social: [
-            {
-                icon: 'fab fa-linkedin-in',
-                url: 'https://www.linkedin.com/in/juanbelmonterodriguez/'
-            },
-            {
-                icon: 'fab fa-github',
-                url: 'https://github.com/jbelmonte'
-            }
-        ]
-    },
-    {
-        name: 'Killian Perea Ruiz',
-        photo: 'assets/img/members/KillianPerea.jpg',
-        description: 'Do not be another brick in the wall',
-        position: 'DEVELOPER',
-        social: [
-            {
-                icon: 'fab fa-linkedin-in',
-                url: 'https://www.linkedin.com/in/killianperea/'
-            },
-            {
-                icon: 'fab fa-github',
-                url: 'https://github.com/killianperea'
-            }
-        ]
-    },
-    {
-        name: 'Raul Fernandez Garcia',
-        photo: 'assets/img/members/RaulFernandez.jpg',
-        description: 'Be prepared',
-        position: 'SCHOLARSHIP HOLDER',
-        social: [
-            {
-                icon: 'fab fa-linkedin-in',
-                url: 'https://www.linkedin.com/in/raulfer/'
-            },
-            {
-                icon: 'fab fa-github',
-                url: 'https://github.com/raul97fernandez'
-            }
-        ]
-    },
-    {
-        name: 'Sergio Calero Robles',
-        photo: 'assets/img/members/SergioCalero.jpg',
-        description: '-It is not possible! -No, it is necessary',
-        position: 'DEVELOPER',
-        social: [
-            {
-                icon: 'fab fa-linkedin-in',
-                url: 'https://www.linkedin.com/in/sergio-calero-robles/'
-            },
-            {
-                icon: 'fab fa-github',
-                url: 'https://github.com/RedParabola'
-            }
-        ]
-    },
-    {
-        name: 'Xavier Cases Camats',
-        photo: 'assets/img/members/XaviCases.jpg',
-        description: 'Be the one',
-        position: 'DEVELOPER',
-        social: [
-            {
-                icon: 'fab fa-linkedin-in',
-                url: 'https://es.linkedin.com/in/xaviercasescamats'
-            },
-            {
-                icon: 'fab fa-github',
-                url: 'https://github.com/xeviscc'
-            }
-        ]
-    }
+  {
+    name: 'Adrian Borrego Martinez',
+    photo: 'assets/img/members/AdrianBorrego.png',
+    description: 'Haters gonna hate',
+    position: 'DEVELOPER',
+    social: [
+      {
+        icon: 'fab fa-github',
+        url: 'https://github.com/adryy5'
+      }
+    ]
+  },
+  {
+    name: 'Alberto Iglesias Gonzalo',
+    photo: 'assets/img/members/AlbertoIglesias.jpg',
+    description: 'Metrosexual part-time and full-time thinker.',
+    position: 'DEVELOPER',
+    social: [
+      {
+        icon: 'fab fa-linkedin-in',
+        url: 'https://www.linkedin.com/in/alberto-iglesias-gonzalo-023511a9/'
+      },
+      {
+        icon: 'fab fa-github',
+        url: 'https://github.com/srkapi'
+      }
+    ]
+  },
+  {
+    name: 'Alejandro Rivas Moreira',
+    photo: 'assets/img/members/AlejandroRivas.jpg',
+    description: 'It is over anakin, I have the high ground',
+    position: 'DEVELOPER',
+    social: [
+      {
+        icon: 'fab fa-github',
+        url: 'https://github.com/AlexHikari/'
+      }
+    ]
+  },
+  {
+    name: 'Alejandro Verbo Macias',
+    photo: 'assets/img/members/AlejandroVerbo.jpg',
+    description: 'There are two rules for success: 1) Never say everything you know',
+    position: 'DEVELOPER',
+    social: [
+      {
+        icon: 'fab fa-linkedin-in',
+        url: 'https://www.linkedin.com/in/alejandro-verbo-0822227/'
+      },
+      {
+        icon: 'fab fa-github',
+        url: 'https://github.com/averbo'
+      }
+    ]
+  },
+  {
+    name: 'Ana Avilés Herrero',
+    photo: 'assets/img/members/AnaAviles.jpg',
+    description: 'No information found',
+    position: 'DEVELOPER',
+    social: [
+      {
+        icon: 'fab fa-linkedin-in',
+        url: 'https://www.linkedin.com/in/ana-avil%C3%A9s-b228079/'
+      },
+      {
+        icon: 'fab fa-github',
+        url: 'https://github.com/aAvilesh'
+      }
+    ]
+  },
+  {
+    name: 'Ana María Castaño Díaz',
+    photo: 'assets/img/members/AnaMariaCastano.png',
+    description: 'My richness is life',
+    position: 'DEVELOPER',
+    social: [
+      {
+        icon: 'fab fa-github',
+        url: 'https://github.com/AnaaMaria25'
+      }
+    ]
+  },
+  {
+    name: 'Andres Iglesias Perez',
+    photo: 'assets/img/members/AndresIglesias.jpg',
+    description: 'There are 2 hard problems in computer science: cache invalidation, naming things, and off-by-1 errors.',
+    position: 'DEVELOPER',
+    social: [
+      {
+        icon: 'fab fa-linkedin-in',
+        url: 'https://www.linkedin.com/in/andresiglesiasperez/'
+      },
+      {
+        icon: 'fab fa-github',
+        url: 'https://github.com/malarres'
+      }
+    ]
+  },
+  {
+    name: 'Cristina Figueres Nuñez',
+    photo: 'assets/img/members/CristinaFigueres.jpg',
+    description: ' I am too old for this shit... ',
+    position: 'DEVELOPER',
+    social: [
+      {
+        icon: 'fab fa-linkedin-in',
+        url: 'https://es.linkedin.com/in/cristinafigueres'
+      },
+      {
+        icon: 'fab fa-github',
+        url: 'https://github.com/cristinafigueres'
+      }
+    ]
+  },
+  {
+    name: 'Dani Bautista Miralles',
+    photo: 'assets/img/members/DaniBautista.png',
+    description: 'There is no spoon',
+    position: 'DEVELOPER',
+    social: [
+      {
+        icon: 'fab fa-linkedin-in',
+        url: 'https://www.linkedin.com/in/daniel-bautista-miralles-14707181/'
+      },
+      {
+        icon: 'fab fa-github',
+        url: 'https://github.com/Dabami'
+      }
+    ]
+  },
+  {
+    name: 'David Castro Fornos',
+    photo: 'assets/img/members/DavidCastro.jpg',
+    description: 'Yo k se no soy 100tifico xddd',
+    position: 'DEVELOPER',
+    social: [
+      {
+        icon: 'fab fa-linkedin-in',
+        url: 'https://www.linkedin.com/in/deivieb/'
+      }
+    ]
+  },
+  {
+    name: 'Enrique Pastor Cisneros',
+    photo: 'assets/img/members/EnriquePastor.jpg',
+    description: 'La respuesta es 42',
+    position: 'DEVELOPER',
+    social: [
+      {
+        icon: 'fab fa-linkedin-in',
+        url: 'https://www.linkedin.com/in/enrique-pastor-cisneros-36571312a/?originalSubdomain=es'
+      },
+      {
+        icon: 'fab fa-github',
+        url: 'https://github.com/epastorc'
+      }
+    ]
+  },
+  {
+    name: 'Eric Álvarez Chinchilla',
+    photo: 'assets/img/members/EricAlvarez.jpg',
+    description: '#nofilter #frontend #developer #worldline',
+    position: 'DEVELOPER',
+    social: [
+      {
+        icon: 'fab fa-linkedin-in',
+        url: 'https://www.linkedin.com/in/eric-alvarez-chinchilla/'
+      },
+      {
+        icon: 'fab fa-github',
+        url: 'https://github.com/ealch'
+      }
+    ]
+  },
+  {
+    name: 'Eric Torres Perramon',
+    photo: 'assets/img/members/EricTorres.png',
+    description: 'Why i\'m still here? just to suffer?',
+    position: 'DEVELOPER',
+    social: [
+      {
+        icon: 'fab fa-linkedin-in',
+        url: 'https://www.linkedin.com/in/etorper/'
+      },
+      {
+        icon: 'fab fa-github',
+        url: 'https://github.com/Erick9Thor'
+      }
+    ]
+  },
+  {
+    name: 'Jairo Costa Barriga',
+    photo: 'assets/img/members/JairoCosta.jpg',
+    description: 'Magic is the science we do not understand yet',
+    position: 'SCHOLARSHIP HOLDER',
+    social: [
+      {
+        icon: 'fab fa-linkedin-in',
+        url: 'https://www.linkedin.com/in/jairocostaes'
+      },
+      {
+        icon: 'fab fa-github',
+        url: 'https://github.com/soyJairoCosta'
+      }
+    ]
+  },
+  {
+    name: 'Jordi Escudero Subirana',
+    photo: 'assets/img/members/JordiEscudero.jpg',
+    description: 'The any type in Typescript was build for me!',
+    position: 'HEAD',
+    social: [
+      {
+        icon: 'fab fa-linkedin-in',
+        url: 'https://www.linkedin.com/in/jordi-escudero/'
+      },
+      {
+        icon: 'fab fa-github',
+        url: 'https://github.com/jordiescudero'
+      }
+    ]
+  },
+  {
+    name: 'Jorge Oíz Acosta',
+    photo: 'assets/img/members/JorgeOiz.jpg',
+    description: 'To change is to progress',
+    position: 'DEVELOPER',
+    social: [
+      {
+        icon: 'fab fa-linkedin-in',
+        url: 'https://www.linkedin.com/in/jorge-oiz-acosta-80a34b57/'
+      },
+      {
+        icon: 'fab fa-github',
+        url: 'https://github.com/JorgeOiz'
+      }
+    ]
+  },
+  {
+    name: 'Jose Luis Pleguezuelos Aguilera',
+    photo: 'assets/img/members/JoseLuisPleguezuelos.jpg',
+    description: 'Do not take life seriously; total, you will not live out of it',
+    position: 'DEVELOPER',
+    social: [
+      {
+        icon: 'fab fa-linkedin-in',
+        url: 'https://www.linkedin.com/in/jose-luis-pleguezuelos-aguilera'
+      }
+    ]
+  },
+  {
+    name: 'Juan Belmonte Rodríguez',
+    photo: 'assets/img/members/JuanBelmonte.jpg',
+    description: '75% water',
+    position: 'DEPUTY',
+    social: [
+      {
+        icon: 'fab fa-linkedin-in',
+        url: 'https://www.linkedin.com/in/juanbelmonterodriguez/'
+      },
+      {
+        icon: 'fab fa-github',
+        url: 'https://github.com/jbelmonte'
+      }
+    ]
+  },
+  {
+    name: 'Killian Perea Ruiz',
+    photo: 'assets/img/members/KillianPerea.jpg',
+    description: 'Do not be another brick in the wall',
+    position: 'DEVELOPER',
+    social: [
+      {
+        icon: 'fab fa-linkedin-in',
+        url: 'https://www.linkedin.com/in/killianperea/'
+      },
+      {
+        icon: 'fab fa-github',
+        url: 'https://github.com/killianperea'
+      }
+    ]
+  },
+  {
+    name: 'Raul Fernandez Garcia',
+    photo: 'assets/img/members/RaulFernandez.jpg',
+    description: 'Be prepared',
+    position: 'SCHOLARSHIP HOLDER',
+    social: [
+      {
+        icon: 'fab fa-linkedin-in',
+        url: 'https://www.linkedin.com/in/raulfer/'
+      },
+      {
+        icon: 'fab fa-github',
+        url: 'https://github.com/raul97fernandez'
+      }
+    ]
+  },
+  {
+    name: 'Sergi Falcón Xarau',
+    photo: 'assets/img/members/SergiFalcón.png',
+    description: '( ͡° ͜ʖ ͡°)',
+    position: 'DEVELOPER',
+    social: [
+      {
+        icon: 'fab fa-linkedin-in',
+        url: 'https://www.linkedin.com/in/sergifalcon/'
+      },
+      {
+        icon: 'fab fa-github',
+        url: 'https://github.com/Sergifx'
+      }
+    ]
+  },
+  {
+    name: 'Xavier Cases Camats',
+    photo: 'assets/img/members/XaviCases.jpg',
+    description: 'Be the one',
+    position: 'DEVELOPER',
+    social: [
+      {
+        icon: 'fab fa-linkedin-in',
+        url: 'https://es.linkedin.com/in/xaviercasescamats'
+      },
+      {
+        icon: 'fab fa-github',
+        url: 'https://github.com/xeviscc'
+      }
+    ]
+  }
 ];
 
 
